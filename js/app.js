c0153405848e6e48ee3b97bf13493225b5c0a46f
Matheus Feo <matheucpsantos@gmail.com>
2018-09-06 18:34:47 -0300

    ADICIONANDO CLASSES AS CARTAS DO TABULEIRO

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
 const listaNo = document.querySelectorAll('.carta');
 const cartaArr= [];
+const arrayClassIcon = [
+  'fa-diamond' , 'fa-paper-plane-o' , 'fa-anchor' , 'fa-bolt' ,
+  'fa-cube' , 'fa-leaf' , 'fa-diamond' , 'fa-paper-plane-o' ,
+  'fa-anchor' , 'fa-bolt' , 'fa-cube' , 'fa-leaf'
+];
 
 listaNo.forEach(function (item,index){
   cartaArr[index] = item;
 });
+
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function atribuiClassesCartas () {
+  shuffle(arrayClassIcon);
+  for(let cont = 0; cont <= cartaArr.length ; cont ++) {
+    cartaArr[cont].querySelector('i').classList.add('fa',arrayClassIcon[cont]);
+  }
+}
+
+atribuiClassesCartas();
